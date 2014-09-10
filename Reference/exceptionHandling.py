571a02409469e233ba7d2c9ac5bb73b0122d3679
Michael Swisher <Michael.V.Swisher@colorado.edu>
2014-09-09 23:56:35 -0600

    Adding a file on basic exception handling to reference material

---
+++ Reference/exceptionHandling.py
@@ -0,0 +1,18 @@
+while True:
+	try:
+		carYear = int(input("What year was your car produced?\n"))
+		print("Your car's production year is %d" % carYear)
+		break
+	except ValueError:
+		print("Make sure you're entering a year...")
+'''
+   except ZeroDivisionError:
+        print("This resulted in a car year of 0...")
+'''
+    except:
+        print("Crashing...")
+        break
+        #This handles all exceptions, but can be counterproductive
+    finally:
+        print("This looped successfully...")
+        #This will run every iteration regardless of exception or not
