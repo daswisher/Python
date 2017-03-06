4c7b16d103323d7dbd1cfdf3c6c56dd6e5ca49b4
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-03-06 09:07:56 -0700

    Updated cropper to maintain image colors.

--- Scripts/ImageScanning/cropper.py
+++ Scripts/ImageScanning/cropper.py
@@ -1,12 +1,24 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('ImageCroppingTest.jpg', 0)
+#img = cv2.imread('ImageCroppingTest.jpg', 0)
+img = cv2.imread('ImageCroppingTest.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-re, thresh = cv2.threshhold(img, 127, 255, 0)
-countours, hierarchy = cv2.findContours(thresh, 1, 2)
+re, thresh = cv2.threshold(gray, 127, 255, 0)
+_, countours, hierarchy = cv2.findContours(thresh, 1, 2)
 
-cnt = contours[0]
+cnt = countours[0]
 
-M = cv2.moments(cn2)
-print M
+M = cv2.moments(cnt)
+#print M
+
+
+rect = cv2.minAreaRect(cnt)
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+cv2.drawContours(img, [box], 0, (0,0,255), 2)
+
+
+cv2.imshow('dst_rt', img)
+cv2.waitKey(0)
