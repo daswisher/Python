111cf5be505b956cb0834218dcb823e0869544f4
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-05-25 23:31:15 -0600

    Beginning server deployment. Adding dispatcher

--- Scripts/CloudAtCost/deployServers.py
+++ Scripts/CloudAtCost/deployServers.py
@@ -1,4 +1,24 @@
-'''
-Refer to:
-https://github.com/cloudatcost/api
-'''
+import json
+
+from cacWrapper import *
+credentialsDict = eval(open('credentials.txt', 'r').read())
+key = credentialsDict['apiKey']
+login = credentialsDict['login']
+
+baseURL = "https://panel.cloudatcost.com"
+
+dispatcher = {
+	'listservers': listServers,
+	'listtemplates': listTemplates,
+	'listtasks': listTasks,
+	'powerop': powerOp,
+	'runmode': runMode,
+	'renameserver': renameServer,
+	'modifydns': modifyDns,
+	'serverconsole': serverConsole,
+	'buildserver': buildServer,
+	'deleteserver': deleteServer,
+	'listresources': listResources
+}
+
+print dispatcher['listresources'](key, login)
