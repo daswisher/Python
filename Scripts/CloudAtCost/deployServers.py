c8f026a6796f53709ed17ca9f6fce5b88328ab6c
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-05-26 00:02:13 -0600

    Successfully retrieving available resources and available templates. Also calculating how to divide resources equally across prospective systems

--- Scripts/CloudAtCost/deployServers.py
+++ Scripts/CloudAtCost/deployServers.py
@@ -21,4 +21,58 @@ dispatcher = {
 	'listresources': listResources
 }
 
-print dispatcher['listresources'](key, login)
+templatesResponse = dispatcher['listtemplates'](key, login)
+while templatesResponse.status_code != 200:
+	templatesResponse = dispatcher['listtemplates'](key, login)
+
+templates = json.loads(templatesResponse.text)['data']
+
+templateID = ''
+templateName = ''
+
+for template in templates:
+	if 'ubuntu' in template['name'].lower():
+		templateID = str(template['ce_id'])
+		templateName = str(template['name'])
+		break
+
+if templateID == '':
+	print "Unable to retrieve Ubuntu template..."
+	exit(1)
+
+response = dispatcher['listresources'](key, login)
+while response.status_code != 200:
+	response = dispatcher['listresources'](key, login)
+
+response = json.loads(response.text)
+dataSet = response['data']
+
+for key in dataSet["used"].keys():
+	try:
+		dataSet["used"][key] = int(dataSet["used"][key])
+
+	except:
+		dataSet["used"][key] = 0
+
+for key in dataSet["total"].keys():
+	try:
+		dataSet["total"][key] = int(dataSet["total"][key])
+
+	except:
+		dataSet["total"][key] = 0
+cpus = dataSet['total']['cpu_total'] - dataSet['used']['cpu_used']
+rams = dataSet['total']['ram_total'] - dataSet['used']['ram_used']
+storages = dataSet['total']['storage_total'] - dataSet['used']['storage_used']
+
+print "cpu:", cpus
+print "ram:", rams
+print "storage:",storages
+
+cpu = 1
+ram = rams/cpus
+storage = storages/cpus
+
+print cpu
+print ram
+print storage
+
