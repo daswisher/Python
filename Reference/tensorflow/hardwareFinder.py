32dfe7f317794643056adf7079d3dc411323eb53
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-05-26 02:14:34 -0600

    Simple 1 liner to get all available hardware available to tensorflow

--- Reference/tensorflow/hardwareFinder.py
+++ Reference/tensorflow/hardwareFinder.py
@@ -1,3 +1,11 @@
 '''
 https://www.tensorflow.org/tutorials/
 '''
+
+
+from tensorflow.python.client import device_lib
+
+local_device_protos = device_lib.list_local_devices()
+print local_device_protos
+#return [x.name for x in local_device_protos if x.device_type == 'GPU']
+
