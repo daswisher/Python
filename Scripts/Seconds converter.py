fe2d03c012142f563715e90abb56d37392cfb219
Michael Swisher <Michael.V.Swisher@colorado.edu>
2017-08-14 10:27:02 -0600

    Organizing

--- Scripts/Seconds converter.py
+++
@@ -1,5 +0,0 @@
-inputSeconds = int(input("Enter a number of seconds to be converted: "))
-hours = int(inputSeconds/3600)
-minutes = int((inputSeconds%3600)/60)
-seconds = inputSeconds % 60
-print("That input is equal to",str(hours),"hours",str(minutes),"minutes and",str(seconds),"seconds.")
