806c5c85fbb19f88363ac87dc41481508e7752e9
Michael Swisher <Michael.V.Swisher@colorado.edu>
2017-11-26 08:14:35 -0700

    Dang white spacing always be getting at me

--- Reference/threading-things/producer-consumer-ref.py
+++ Reference/threading-things/producer-consumer-ref.py
@@ -6,24 +6,24 @@ from Queue import Queue
 queue = Queue(10)
 
 class ProducerThread(Thread):
-    def run(self):
-        nums = range(5)
-        global queue
-        while True:
-            num = random.choice(nums)
-            queue.put(num)
-            print "Produced", num
-            #time.sleep(random.random())
+	def run(self):
+		nums = range(5)
+		global queue
+		while True:
+			num = random.choice(nums)
+			queue.put(num)
+			print "Produced", num
+			#time.sleep(random.random())
 
 
 class ConsumerThread(Thread):
-    def run(self):
-        global queue
-        while True:
-            num = queue.get()
-            queue.task_done()
-            print "Consumed", num
-            time.sleep(3)
+	def run(self):
+		global queue
+		while True:
+			num = queue.get()
+			queue.task_done()
+			print "Consumed", num
+			time.sleep(3)
 
 
 ProducerThread().start()
